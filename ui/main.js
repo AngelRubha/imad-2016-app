f12f0244ee195d4b27b9c1fd42d9a3cfb9936914
AngelRubha <angelrubha472@gmail.com>
2016-09-23 21:12:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,8 +2,8 @@ console.log('Loaded!');
 var image=document.getElementById("img");
 //var marginLeft=0;
 //function moveRight() {
-    marginLeft=marginLeft+5;
-    image.style.marginLeft=marginLeft+'px';
+  //  marginLeft=marginLeft+5;
+  //  image.style.marginLeft=marginLeft+'px';
 }//
 image.onclick=function() {
    // var interval=setInterval(moveRight,50);
